ce6cc23978f825dcf885424a4bcc8f87ebe29895
phmanhs <149356601+phmanhs@users.noreply.github.com>
2024-07-01 22:18:01 +0700

    Add Download Pages

---
+++ html/js/script.js
@@ -0,0 +1,4 @@
+// Tệp JavaScript cho trang web (nếu cần thêm chức năng nào đó)
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Trang đã tải xong và sẵn sàng hoạt động!");
+});
